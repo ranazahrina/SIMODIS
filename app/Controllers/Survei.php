015c1a64b94ee80c3504ad1071db90aa8ed140da
Muhammad Rizky Pratama <50003993+Tamagz@users.noreply.github.com>
2021-03-03 12:52:37 +0700

    SEMUA FUNGSI TAMBAH DATA
    masih acak-acakan uinya terus validasi data belom diterapin

--- app/Controllers/Survei.php
+++ app/Controllers/Survei.php
@@ -2,6 +2,7 @@
 
 namespace App\Controllers;
 
+use App\Models\dataJenisSurveyModel;
 use \App\Models\datasurveyModel;
 use CodeIgniter\CodeIgniter;
 
@@ -9,9 +10,11 @@ class Survei extends BaseController
 {
 
     protected $data;
+    protected $databps;
     public function __construct()
     {
         $this->data = new datasurveyModel();
+        $this->databps = new dataJenisSurveyModel();
     }
 
     public function test()
@@ -45,7 +48,36 @@ class Survei extends BaseController
         return redirect()->to('/home/datamasuk');
     }
 
-    // public function edit($id){
-    //     $data 
-    // }
+    public function edit($id)
+    {
+
+        $isidata = $this->data->find($id);
+        $survey = $this->databps->findAll();
+
+        $data = [
+            'tittle' => 'Tab edit',
+            'data' => $isidata,
+            'jenis' => $survey
+        ];
+
+        echo view('layout/header', $data);
+        echo view('layout/sidebar');
+        echo view('layout/topbar');
+        echo view('home/editmasuk', $data);
+        echo view('layout/footer');
+    }
+
+    public function update($id)
+    {
+        $request = service('request');
+        $this->data->save([
+            'id' => $id,
+            'jenis_survey' => $request->getVar('jenis_survey'),
+            'responden' => $request->getVar('responden'),
+            'waktu_pelaksanaan' => $request->getVar('pelaksanaan'),
+            'waktu_survey' => $request->getVar('waktu_s'),
+            'nama_petugas' => $request->getVar('nama_petugas')
+        ]);
+        return redirect()->to('/home/datamasuk');
+    }
 }
---
+++ app/Views/home/editmasuk.php
@@ -0,0 +1,67 @@
+<div class="container">
+    <div class="row">
+        <div class="col">
+            <h1 class="mt-2">Edit data</h1>
+            <form action="/survei/update/<?= $data['id']; ?>" method="post">
+
+                <?= csrf_field(); ?>
+                <div class="col-md-2">
+                    <label>Jenis Survey</label>
+                    <select class="form-control" id="jenis_survey" name="jenis_survey" placeholder="<?= $data['jenis_survey']; ?>">
+                        <option disabled selected="<?= $data['jenis_survey']; ?>"> <?= $data['jenis_survey']; ?></option>
+                        <?php foreach ($jenis as $a) : ?>
+
+                            <option value="<?= $a['jenis_survey']; ?>"> <?= $a['jenis_survey']; ?> </option>
+                        <?php endforeach; ?>
+                    </select>
+                </div>
+
+                <div class="col-md-2">
+                    <label>Waktu Survey</label>
+                    <select class="form-control" id="waktu_s" name="waktu_s" placeholder="Waktu Survey">
+                        <option disabled selected="<?= $data['waktu_survey']; ?>"><?= $data['waktu_survey']; ?></option>
+                        <option value="Bulanan">Bulanan</option>
+                        <option value="Triwulan">Triwulan</option>
+                        <option value="Tahunan">Tahunan</option>
+                    </select>
+                </div>
+                <div class="col-md-2">
+                    <label>Pelaksanaan</label>
+                    <select class="form-control" id="pelaksanaan" name="pelaksanaan" placeholder="Waktu Pelaksanaan">
+                        <option disabled selected="<?= $data['waktu_pelaksanaan']; ?>"><?= $data['waktu_pelaksanaan']; ?></option>
+                        <option value=" JANUARI">JANUARI</option>
+                        <option value="FEBRUARI">FEBRUARI</option>
+                        <option value="MARET">MARET</option>
+                        <option value="APRIL">APRIL</option>
+                        <option value="MEI">MEI</option>
+                        <option value="JUNI">JUNI</option>
+                        <option value="JULI">JULI</option>
+                        <option value="AGUSTUS">AGUSTUS</option>
+                        <option value="SEPTEMBER">SEPTEMBER</option>
+                        <option value="OKTOBER">OKTOBER</option>
+                        <option value="NOVEMBER">NOVEMBER</option>
+                        <option value="DESEMBER">DESEMBER</option>
+                    </select>
+                </div>
+                <div class="col-md-2">
+                    <label>Petugas</label>
+                    <input type="text" name="nama_petugas" class="form-control" placeholder=" <?= $data['nama_petugas']; ?>">
+                </div>
+
+                <div class="col-md-2">
+                    <label>Responden</label>
+                    <input type="text" name="responden" class="form-control" placeholder="  <?= $data['responden']; ?>">
+                </div>
+                <div class="col-md-2">
+                    <button type="submit" name="submit" class="btn btn-primary btn-block" onchange="this.form.submit()"> submit</button>
+                </div>
+            </form>
+            <?php if (session()->getFlashdata('berhasil')) : ?>
+                <div class="alert alert-danger" role="alert">
+                    <?= session()->getFlashdata('berhasil'); ?>
+                </div>
+            <?php endif; ?>
+        </div>
+
+    </div>
+</div>
