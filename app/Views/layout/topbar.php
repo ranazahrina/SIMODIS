3f42672a3d12b450bf28caf7367daed52b3930ac
Muhammad Rizky Pratama <50003993+Tamagz@users.noreply.github.com>
2021-03-07 13:18:26 +0700

    UPDATE LOGIN MYAUTH
    login dah bisa diimplementasi template masih default

--- app/Views/layout/topbar.php
+++ app/Views/layout/topbar.php
@@ -20,18 +20,25 @@
                 <!-- Nav Item - User Information -->
                 <li class="nav-item dropdown no-arrow">
                     <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                        <span class="mr-2 d-none d-lg-inline text-gray-600 small">Douglas McGee</span>
+                        <span class="mr-2 d-none d-lg-inline text-gray-600 small">Anonn</span>
+
                         <img class="img-profile rounded-circle" style="height:35px" src="<?php echo base_url(); ?>/assets/img/profile-user.png">
                     </a>
+
                     <!-- Dropdown - User Information -->
                     <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in" aria-labelledby="userDropdown">
                         <a class="dropdown-item" href="#" data-toggle="modal" data-target="#logoutModal">
-                            <i class="fas fa-sign-out-alt fa-sm fa-fw mr-2 text-gray-400"></i>
-                            Logout
+                            <i class="fas fa-sign-out-alt fa-sm fa-fw mr-2 text-gray-400">Logout</i>
+
                         </a>
+
                     </div>
                 </li>
-
+                <?php if (logged_in()) : ?>
+                    <a href="/logout">Login</a>
+                <?php else : ?>
+                    <a href="/logout">Logout</a>
+                <?php endif; ?>
             </ul>
 
         </nav>
