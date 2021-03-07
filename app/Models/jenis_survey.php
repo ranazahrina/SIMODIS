f47eea5a4b228e2a5bfb63ee075ed9f0c12f5f8b
Febiola Agatha <50370849+febiolagatha@users.noreply.github.com>
2021-03-07 21:06:12 +0700

    Perbaikan Tampilan
    Login,Register,Logout

---
+++ app/Models/jenis_survey.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use CodeIgniter\Model;
+
+class jenis_survey extends Model
+{
+    protected $table = 'jenis_survey';
+    protected $allowedFields = ['jenis_survey'];
+}
--- app/Views/layout/topbar.php
+++ app/Views/layout/topbar.php
@@ -21,24 +21,21 @@
                 <li class="nav-item dropdown no-arrow">
                     <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                         <span class="mr-2 d-none d-lg-inline text-gray-600 small">User</span>
-
                         <img class="img-profile rounded-circle" style="height:35px" src="<?php echo base_url(); ?>/assets/img/profile-user.png">
-
-                        <!-- Dropdown - User Information -->
-                        <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in" aria-labelledby="userDropdown">
-                            <a class="dropdown-item" href="#" data-toggle="modal" data-target="#logoutModal">
-                                <i class="fas fa-sign-out-alt fa-sm fa-fw mr-2 text-gray-400">Logout</i>
-                            </a>
-
-                        </div>
                     </a>
-
+                    <!-- Dropdown - User Information -->
+                    <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in" aria-labelledby="userDropdown">
+                        <?php if (logged_in()) : ?>
+                            <a class="dropdown-item" href="/logout">
+                                <i class="fas fa-sign-out-alt fa-sm fa-fw mr-2 text-gray-400"></i>
+                                Logout
+                            </a>
+                        <?php else : ?>
+                            <a href="/login">Login</a>
+                        <?php endif; ?>
+                    </div>
+                    </a>
                 </li>
-                <?php if (logged_in()) : ?>
-                    <a href="/logout">Logout</a>
-                <?php else : ?>
-                    <a href="/login">Login</a>
-                <?php endif; ?>
             </ul>
 
         </nav>
